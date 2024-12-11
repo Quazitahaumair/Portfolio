6bcfc4beee098241206d7ae3fc3041a6f38a293b
quazitaha <quaitahaumair92@gmail.com>
2024-12-12 00:19:15 +0530

    initialized repo

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,35 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/quazi.taha.9?mibextid=ZbWKwL',
+  instagram: 'https://www.instagram.com/im.quazi_taha',
+  twitter: 'https://twitter.com/Quazitahaumair',
+  github: 'https://github.com/',
+  linkedin: 'https://www.linkedin.com/in/quazi-taha-5bb564269',
+};
+
+export const author = {
+  name: 'Quazi Taha',
+  email: 'quazitahaumair92@gmail.com',
+};
+
+export const seoData = {
+  title: 'Quazi Taha | Front-End Developer',
+  description:
+    'Quazi Taha is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://singhkv.vercel.app/',
+  keywords: [
+    'Quazi Taha',
+    'Portfolio',
+    'Quazi Taha Portfolio',
+  ],
+};
---
+++ src/lib/content/projects.ts
@@ -0,0 +1,8 @@
+import { ProjectsSectionType } from '@/lib/types/sections';
+import { getId } from '@/lib/utils/helper';
+
+export const projectsSection: ProjectsSectionType = {
+  title: 'my projects',
+  projects: [
+  ],
+};
